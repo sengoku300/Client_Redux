014dda4a0e693466653048133ed83d4175a771bf
bioiiiokolat@gmail.com <bioiiiokolat@gmail.com>
2021-04-20 14:57:45 +0300

    Привязал Service Reference, добавил функционал с сервисом в приложении.
    Немного изменён содержимое папки Pages.

--- WPF_Redux_Client/Pages/RegPage.xaml.cs
+++ WPF_Redux_Client/Pages/RegPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Redux_Client.ServiceReference1;
 
 namespace WPF_Redux_Client.Pages
 {
@@ -23,12 +25,22 @@ namespace WPF_Redux_Client.Pages
     /// </summary>
     public partial class RegPage : Page
     {
+        private string gender;
+
+        Service1Client client;
+
+        Regex regex = new Regex("[^0-9]+");
+
         public RegPage()
         {
             InitializeComponent();
 
             city_name.Text = GetCity();
             country_name.Text = GetCountry();
+
+            textBox_Birthday_DD.email.MaxLength = 2;
+            textBox_Birthday_MM.email.MaxLength = 2;
+            textBox_Birthday_Year.email.MaxLength = 4;
         }
 
         Authorization authoriz { get => Application.Current.MainWindow as Authorization; }
@@ -78,24 +90,41 @@ namespace WPF_Redux_Client.Pages
             return ipLoc;
         }  
         
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click_1(object sender, RoutedEventArgs e) => gender = "Мужчина";
 
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
+        private void Button_Click_2(object sender, RoutedEventArgs e) => gender = "Женщина";
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            authoriz.authMain.Navigate(new Uri("Pages/LoginPage.xaml", UriKind.RelativeOrAbsolute));
+            if (Int32.TryParse(textBox_Birthday_DD.Text, out int dd)
+                && Int32.TryParse(textBox_Birthday_MM.Text, out int mm)
+                && Int32.TryParse(textBox_Birthday_Year.Text, out int year)
+                && textBox_Mail.Text.Contains("@")
+                && !string.IsNullOrEmpty(textBox_Name_Family.Text)
+                && passBox.passbox.Password != ""
+                && gender != "")
+            {
+                client = new Service1Client();
+
+                string birthday = dd + "." + mm + "." + year;
+
+                DateTime date = DateTime.Parse(birthday);
+
+                client.AddAccount(textBox_Mail.Text, passBox.passbox.Password,
+                    textBox_Name_Family.Text, city_name.Text, country_name.Text, date, gender);
+
+                authoriz.authMain.Navigate(new Uri("Pages/LoginPage.xaml", UriKind.RelativeOrAbsolute));
+            }
         }
+
+
+        private void textBox_Birthday_DD_PreviewTextInput(object sender, TextCompositionEventArgs e) 
+            => e.Handled = regex.IsMatch(e.Text);
+
+        private void textBox_Birthday_MM_PreviewTextInput(object sender, TextCompositionEventArgs e)
+            => e.Handled = regex.IsMatch(e.Text);
+
+        private void textBox_Birthday_Year_PreviewTextInput(object sender, TextCompositionEventArgs e)
+            => e.Handled = regex.IsMatch(e.Text);
     }
 }
